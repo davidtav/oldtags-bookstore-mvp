81e9ff339e886191c6f76e79ddab05aab88549fb
davidtav <david.mclaurel@outlook.com.br>
2025-11-13 11:32:23 -0300

    feat: inserção de csrf

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,13 +3,10 @@
 
 <head>
     <title>OldTags Bookstore</title>
-
+    <meta name="csrf-token" content="{{ csrf_token() }}">    
     <script src="https://cdn.tailwindcss.com"></script>
-
     <script src="https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2"></script>
-
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
-
 </head>
 
 <body class="text-gray-800 bg-white">
